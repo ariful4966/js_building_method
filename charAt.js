859119cf85e1803dd57a8613de403d873d09a87b
nodeAriful <ariful.node82@gmail.com>
2020-12-21 02:09:33 +0600

    chartAt and concate method is discuses

---
+++ charAt.js
@@ -0,0 +1,6 @@
+const sentence = "The quick brown fox jumps over the lazy dog";
+
+const index = 4;
+
+const chart = ` The Character at index ${index} is ${sentence.charAt(index)}`;
+console.log(chart);
---
+++ concat.js
@@ -0,0 +1,9 @@
+const love = 'I love you,';
+const land = 'Oh! my motherland';
+const bangla ='is Bangladesh'
+
+const concat1 = love.concat('', land)
+const concat2 = concat1.concat(', ', bangla)
+
+console.log(concat1);
+console.log(concat2);
