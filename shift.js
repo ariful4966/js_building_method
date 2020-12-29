d7047adc4c3ffe2be270b6aeac0c979954147425
nodeAriful <ariful.node82@gmail.com>
2020-12-29 23:48:51 +0600

    reduce reverce shift and unshift is done

---
+++ shift.js
@@ -0,0 +1,7 @@
+const array = [1, 2, 3, 4, 5];
+
+const firstElement = array.shift();
+
+console.log(array);
+
+console.log(firstElement);
---
+++ unshift.js
@@ -0,0 +1,5 @@
+const array = [1, 3, 4, 5, 8];
+
+console.log(array.unshift(9, 7));
+
+console.log(array);
